0ea58db5b4bb3633c12fa77aaf5247b58947798c
Lin Wei-Chih <34365220+kjoelovelife@users.noreply.github.com>
2019-01-28 08:56:32 +0000

    Create voice_control.py

---
+++ catkin_ws/src/pocketsphinx/scripts/voice_control.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding=UTF-8
+
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+
+
+
+## set key word list
+key_word = {
+             "go ":(0,30,0,0,0,0),
+             "stop ":(0,0,0,0,0,0),
+             "back ":(0,-30,0,0,0,0)
+           }
+
+
+def callback(data):
+    word = data.data
+    if word.find( str(key_word.keys()) ):    
+        print(word)
+        twist.linear.x = key_word[word][0]
+        twist.linear.y = key_word[word][1]
+        twist.linear.z = key_word[word][2]
+        twist.angular.x = key_word[word][3]
+        twist.angular.y = key_word[word][4]
+        twist.angular.z = key_word[word][5]
+        pub.publish(twist)
+
+if __name__ == '__main__':
+
+    ## set Twist
+    twist = Twist()
+    twist.linear.x = 0  
+    twist.linear.y = 0
+    twist.linear.z = 0
+    twist.angular.x = 0
+    twist.angular.y = 0
+    twist.angular.z = 0
+
+    ## initialize
+    rospy.init_node('voice_control', anonymous=True)
+    pub = rospy.Publisher('/cmd_vel', Twist, queue_size = 1)
+    while(True):       
+        if rospy.is_shutdown() == True:
+            print''
+            break
+        else:
+            rospy.Subscriber('pocketsphinx_recognizer/output', String, callback)
+            rospy.spin()
